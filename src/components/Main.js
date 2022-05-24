57291a8d2e3e3e77987e0857d461b8cd1285be61
mstars_lab_04 <mstars_lab_04@mstars-lab-04s-iMac.local>
2022-05-24 14:09:08 +0800

    new file uploaded

---
+++ src/components/Main.js
@@ -0,0 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function Main() {
+  return <div>Main</div>;
+}
